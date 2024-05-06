394be435eb0ad966423f4acc69c046e94760c459
tsubasa <tsubasa@tsubasanoMacBook-Air.local>
2024-05-06 22:44:59 +0900

    M1mac 5/6実装分

--- frontend/app/src/Works/components/WorkItem/index.tsx
+++ frontend/app/src/Works/components/WorkItem/index.tsx
@@ -1,18 +1,31 @@
 import { WorkItemData } from "./interface";
 import { GoGear, GoFileCode, GoLinkExternal } from "react-icons/go";
-import { styled } from "styled-components"
+import { styled } from "styled-components";
+import image01 from "images/image01.jpg";
 
 const WorkItemBlock = styled.div`
-  border: 1px solid #BEBEBE;
+  border: 1px solid #f0efee;
   border-radius: 12px;
   padding: 16px 24px;
+  background: #FBF9FA;
+  display: flex;
+  gap: 16px;
   svg {
     margin-right: 4px;
   }
 `;
 
+const ImageBlock = styled.div`
+  width: 200px;
+  img {
+    width: 100%;
+  }
+`;
+
 const WorkItemHeader = styled.div`
   margin-bottom: 8px;
+  display: flex;
+  align-items: center;
 `;
 
 const Title = styled.span`
@@ -20,8 +33,16 @@ const Title = styled.span`
   font-size: 24px;
 `;
 
+const WorkItemLink = styled.a`
+  color: #404040;
+`;
+
 const WorkingProcess = styled.span`
   margin-right: 24px;
+`;
+
+const Discription = styled.div`
+  margin-top: 12px;
 `;
 
 interface Props {
@@ -32,29 +53,34 @@ interface Props {
 export const WorkItem: React.FC<Props> = (props) => {
   return (
     <WorkItemBlock className={props.className}>
-      <WorkItemHeader>
-        <Title>
-          {props.workItem.title}
-        </Title>
-        {
-          props.workItem.url && <span>
-            <GoLinkExternal />
-            <a href={props.workItem.url} target="_blank"  rel="noreferrer">
-              {props.workItem.url}
-            </a>
-          </span>
-        }
-      </WorkItemHeader>
-      <WorkingProcess>
-        <GoGear />
-        {props.workItem.workingProcess.join(' / ')}
-      </WorkingProcess>
-      <span>
-        <GoFileCode />
-        {props.workItem.technology.join(' / ')}
-      </span>
+      <ImageBlock>
+        <img src={image01}></img>
+      </ImageBlock>
       <div>
-        {props.workItem.description}
+        <WorkItemHeader>
+          <Title>
+            {props.workItem.title}
+          </Title>
+          {
+            props.workItem.url && <span>
+              <WorkItemLink href={props.workItem.url} target="_blank"  rel="noreferrer">
+                <GoLinkExternal style={{verticalAlign: "bottom"}} />
+                {props.workItem.url}
+              </WorkItemLink>
+            </span>
+          }
+        </WorkItemHeader>
+        <WorkingProcess>
+          <GoGear />
+          {props.workItem.workingProcess.join(' / ')}
+        </WorkingProcess>
+        <span>
+          <GoFileCode />
+          {props.workItem.technology.join(' / ')}
+        </span>
+        <Discription>
+          {props.workItem.description}
+        </Discription>
       </div>
     </WorkItemBlock>
   );
---
+++ frontend/app/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { styled } from "styled-components";
+
+const HeaderSyle = styled.header`
+  position: fixed;
+  top: 0;
+  height: 64px;
+  width: 100%;
+  background: #FBF9FA;
+  display: flex;
+`;
+
+export const Header: React.FC = () => {
+  return (
+    <HeaderSyle >
+
+    </HeaderSyle>
+  );
+}
